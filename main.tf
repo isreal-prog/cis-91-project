166134ceb56b3f4eb6b10d58f8edb354106b2e8b
Isreal Longoria <isreal.g.longoria@gmail.com>
2025-12-12 06:06:17 +0000

    updated my work

--- main.tf
+++ main.tf
@@ -17,35 +17,62 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
-resource "google_compute_firewall" "allow_web_traffic" {
-  name    = "allow-web-traffic"
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
   network = google_compute_network.vpc_network.name
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443"]
+    ports    = ["80", "443"]
   }
 
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["web"]
 }
 
+  resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["web","db"]
+  }
+  
+resource "google_compute_firewall" "allow_db" {
+  name    = "allow-db"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3306"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["db"]
+}
+
+
 resource "google_service_account" "vm_sa" {
   account_id   = "vm-instance-sa"
   display_name = "VM Instance Service Account"
 }
 
-resource "google_compute_disk" "instance_disk" {
-  name = "terraform-instance-disk"
+resource "google_compute_disk" "persistent_disk" {
+  name = "persistent-disk"
   type = "pd-balanced"
   zone = var.zone
   size = 10
 }
 
-resource "google_compute_instance" "vm_instance"{
-  name         = "terraform-instance"
+resource "google_compute_instance" "db_instance"{
+  name         = "db-instance"
 machine_type = "e2-small"
-tags         = ["web", "dev"]
+tags         = ["db"]
 allow_stopping_for_update = true
 
   boot_disk {
@@ -56,7 +83,8 @@ allow_stopping_for_update = true
   }
 
   attached_disk {
-    source = google_compute_disk.instance_disk.id
+    source = google_compute_disk.persistent_disk.id
+  device_name = "persistent-disk-1"
   }
 
   network_interface {
@@ -73,11 +101,35 @@ allow_stopping_for_update = true
   }
 }
 
-output "ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.network_ip
-}
+resource "google_compute_instance" "web_instance"{
+  name         = "web-instance"
+machine_type = "e2-small"
+tags         = ["web"]
+allow_stopping_for_update = true
 
+  boot_disk {
+    initialize_params {
+  image = "debian-cloud/debian-12"
+  
+     }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+   }
+
+
+output "db-ip" {
+  value = google_compute_instance.db_instance.network_interface.0.network_ip
+}
+output "web-ip" {
+  value = google_compute_instance.web_instance.network_interface.0.network_ip
+}
 output "external_ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.web_instance.network_interface.0.access_config.0.nat_ip
 
 }
+
